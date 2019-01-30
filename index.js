ee7ce42609e82c614a1779960ddfd05c344b54b3
Nezar Tarbin <nizar.t96@gmail.com>
2019-01-30 00:01:59 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+//importing express and bodyparsers
+const express = require("express");
+const bodyParser = require('body-parser');
+
+//initializing express app
+const app = express();
+
+//mounting bodyparser
+//bodyParser will parse HTTP request body for us
+app.use(express.json());
+
+//define main get request. When accessing root, we send our home HTML page.
+app.get("/", (req, res, next) => {
+    res.sendFile(__dirname + "/public/home.html", () => console.log("home page sent"));
+});
+
+//When CSS stylesheet requested, we send it
+app.get("/css/main.css", (req,res,next) => {
+    res.sendFile(__dirname + "/public/css/main.css");
+});
+
+//handling post request
+app.post('/user', (req,res,next) => {
+    console.log(req.body);
+});
+
+//defining server listener
+app.listen(3000, () => {
+    console.log(`Server started on port 3000...`);
+});
